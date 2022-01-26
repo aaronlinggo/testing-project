9d9cc19056b7c5f03d02a3dae0e77d646bbfcc29
Kenny <kennylink2626@gmail.com>
2022-01-26 13:29:27 +0800

    fixed tailwind dependency

--- blog-client/src/App.vue
+++ blog-client/src/App.vue
@@ -1,24 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <!-- <h1 class="text-9xl bg-indigo-500">Hello World</h1>   -->
+  <Navbar id="nav"/>
   <router-view/>
 </template>
 
+<script>
+  import Navbar from '@/components/Navbar.vue'
+  export default {
+    name: 'Home',
+    components: {
+      Navbar
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
