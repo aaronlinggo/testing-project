195f67513e26c410f232784d819d963978b46c3c
Aaron Linggo Satria <aaronlinggosatria@gmail.com>
2022-02-05 16:44:42 +0700

    login api done

--- blog-api/routes/api.php
+++ blog-api/routes/api.php
@@ -18,8 +18,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group([
+    'prefix' => 'auth'
+], function (){
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('signup', 'App\Http\Controllers\AuthController@signup');
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function(){
+        Route::get('logout', 'App\Http\Controllers\AuthController@logout');
+        Route::get('user', 'App\Http\Controllers\AuthController@user');
+    });
 });
 
 // get api blog
